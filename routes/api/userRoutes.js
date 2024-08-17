38ef93b5ac72d795e2d45e6b5d049f7c5ba913f5
JGuzman87 <jguzman913@gmail.com>
2024-08-17 12:48:53 -0400

    completed schemas, started routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { 
+    getUsers,
+    getSingleUser, 
+    createUser, 
+    updateUser,
+    deleteUser,
+ } = require('../../controllers/userController');
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userID').get(getSingleUser)
+
+router
+.route('/:userID')
+.get(getSingleUser)
+.put(updateUser)
+.delete(deleteUser);
+
+module.exports = router;
